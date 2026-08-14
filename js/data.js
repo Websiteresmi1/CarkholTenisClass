114cd48a27c3770e6fb3b1b04efea62b64bc207e
xiro113 <akunrahmat.baru@gmail.com>
2026-08-14 17:11:49 +0700

    file pertama

--- js/data.js
+++ js/data.js
@@ -28,7 +28,7 @@ const TENNIS_DATA = {
   coach: {
     name: "Rizaldy Maulidan Firdaus",
     title: "Head Coach & Founder Carkhol Tenis Class",
-    image: "assets/images/Profil Zaldy.png",
+    image: "assets/images/zaldy.png",
     certification: "Badash & Pourkelapb Sadish",
     experience: "12+ Tahun Pengalaman Kepelatihan",
     bio: "Coach Zaldy memadukan pengalaman bertanding di tingkat nasional dengan pendekatan ilmiah modern berbasis bio-mekanika gerakan tenis dan ketahanan mental bertanding.",
